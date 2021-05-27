014318328444451e8e28c4507e6d3cadfd4b03a4
Anuken <arnukren@gmail.com>
2021-05-26 22:37:07 -0400

    Progress

--- src/nimovi.nim
+++ src/nimovi.nim
@@ -1,10 +1,17 @@
 import fcore, ui/ui
 
 #enable once UI becomes a thing
-#static: echo staticExec("faupack -p:../assets-raw/sprites -o:../assets/atlas")
+static: echo staticExec("faupack -s -p:../assets-raw/sprites -o:../assets/atlas")
+
+const text = ["do not click", "5", "4", "3", "2", "1", "nothing"]
+var i = 0
 
 proc init() =
   echo "Nimovi: init()"
+  uiPatchScale = 5f
+  defaultButtonStyle = ButtonStyle(up: "button".patch9, down: "button-down".patch9, overColor: rgba(1, 1, 1, 0.2f))
+  defaultFont = loadFont("font.ttf")
+  uiFontScale = 3f
 
 proc run() =
   if keyEscape.tapped: quitApp()
@@ -13,7 +20,8 @@ proc run() =
 
   lineRect(0, 0, fau.widthf, fau.heightf, color = rgb(0, 1, 1), stroke = 5f)
 
-  if button(rect(fau.width/2f, fau.heightf/2f, 300, 60), region = "circle".patch):
+  if button(rect(fau.width/2f, fau.heightf/2f, 340, 60), text[i]):
     echo "button pressed"
+    i.inc
 
 initFau(run, init, windowTitle = "nimovi")
